6da64a508140aa416fbf92130b6ac683b1ba2348
Jessica Bernardo <jessicaaurorabernardo@gmail.com>
2024-06-08 16:50:39 -0300

    AulaS2

---
+++ media.py
@@ -0,0 +1,13 @@
+# criando a função
+def media(nota1, nota2):
+    resultadoMedia = (nota1 + nota2) / 2
+    print('Nota 1:', nota1)
+    print('Nota 2:', nota2)
+    print('A média das notas é:', resultadoMedia)
+
+# Definindo as notas [variáveis]
+nota1 = 8
+nota2 = 9
+
+# Chamando a função média
+media(nota1, nota2)
